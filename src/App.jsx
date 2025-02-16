561c07f3ffc1676cc4ff51054721d9f0623907c7
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-02-17 02:58:03 +0530

    add useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 
 function App() {
 
+
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hi, Abdul Rahman</h1>
